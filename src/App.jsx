de880be6778cd442207d96b726b56950ce20185a
Valery Subbotin <subbotin.valery.evgenyevich@gmail.com>
2021-03-08 02:41:13 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,11 @@ class App extends Component{
     render(){
         return(
             <BrowserRouter>
-                <Header/>
-                <CardsBlock/>
-                <Footer/>
+                <div className="wrapper">
+                    <Header/>
+                    <CardsBlock/>
+                    <Footer/>
+                </div>  
             </BrowserRouter>
         );
     }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fab);
 
 const Footer = () => (
-    <footer>
-    </footer>
+    <div className="footer-wrapper">
+        <footer>
+            <div className="copyright">
+                <span>Company Name &copy; 2021</span>
+            </div>
+            <div className="social">
+                <a href="#"><FontAwesomeIcon icon={['fab', 'instagram']} size="lg" fixedWidth /></a>
+                <a href="#"><FontAwesomeIcon icon={['fab', 'facebook']} size="lg" fixedWidth /></a>
+                <a href="#"><FontAwesomeIcon icon={['fab', 'google']} size="lg" fixedWidth /></a>
+                <a href="#"><FontAwesomeIcon icon={['fab', 'vk']} size="lg" fixedWidth /></a>
+            </div>
+        </footer>
+    </div>
 );
 
 export default Footer;
