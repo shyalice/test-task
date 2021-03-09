52652c2f3a4daf0ee87c9a5f6e4fd531f439ff3f
Valery Subbotin <subbotin.valery.evgenyevich@gmail.com>
2021-03-09 16:41:35 +0300

    finish footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,19 +6,17 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 library.add(fab);
 
 const Footer = () => (
-    <div className="footer-wrapper">
-        <footer>
-            <div className="copyright">
-                <span>Company Name &copy; 2021</span>
-            </div>
-            <div className="social">
-                <a href="#instagram"><FontAwesomeIcon icon={['fab', 'instagram']} size="lg" fixedWidth /></a>
-                <a href="#facebook"><FontAwesomeIcon icon={['fab', 'facebook']} size="lg" fixedWidth /></a>
-                <a href="#google"><FontAwesomeIcon icon={['fab', 'google']} size="lg" fixedWidth /></a>
-                <a href="#vk"><FontAwesomeIcon icon={['fab', 'vk']} size="lg" fixedWidth /></a>
-            </div>
-        </footer>
-    </div>
+    <footer className="footer">
+        <div className="copyright">
+            <span>Company Name &copy; 2021</span>
+        </div>
+        <div className="social">
+            <a href="#instagram"><FontAwesomeIcon icon={['fab', 'instagram']} size="lg" fixedWidth /></a>
+            <a href="#facebook"><FontAwesomeIcon icon={['fab', 'facebook']} size="lg" fixedWidth /></a>
+            <a href="#google"><FontAwesomeIcon icon={['fab', 'google']} size="lg" fixedWidth /></a>
+            <a href="#vk"><FontAwesomeIcon icon={['fab', 'vk']} size="lg" fixedWidth /></a>
+        </div>
+    </footer>
 );
 
 export default Footer;
